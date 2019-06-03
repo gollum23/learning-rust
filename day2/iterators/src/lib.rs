40011ebf1226a998e5bfc0c9a13363400612d8db
Diego Forero <gollum23@gmail.com>
2019-06-03 06:30:11 -0500

    Generic iterators

---
+++ day2/iterators/src/lib.rs
@@ -0,0 +1,78 @@
+use std::ops::AddAssign;
+use std::cmp::PartialOrd;
+
+
+pub struct Stepper<T> {
+    curr: T,
+    step: T,
+    stop: T,
+}
+
+impl<T> Stepper<T> {
+    pub fn new(start:T, stop:T, step:T) -> Self {
+        Stepper {
+            curr:start,
+            stop:stop,
+            step:step,
+        }
+    }
+}
+
+impl<T> Iterator for Stepper<T>
+    where T:AddAssign + Copy + PartialOrd
+{
+    type Item=T;
+
+    fn next(&mut self) -> Option<T> {
+        if self.curr >= self.stop {
+            return None;
+        }
+        let res = self.curr;
+        self.curr += self.step;
+        Some(res)
+    }
+}
+
+fn sum_list<I, S>(l:I, mut s:S) -> S
+    where I:Iterator<Item=S>, S:AddAssign
+{
+    for n in l {
+        s += n;
+    }
+    s
+
+}
+
+fn sum_list2<I, S>(l:I, mut s:S) -> S
+    where I:IntoIterator<Item=S>, S:AddAssign
+{
+
+    let mut it = l.into_iter();
+    while let Some(n) = it.next() {
+        s += n;
+    }
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut c = 0;
+        for n in Stepper::new(2, 10, 2) {
+            c += n;
+        }
+
+        assert_eq!(c, 20);
+
+        let sl = sum_list(Stepper::new(3, 10, 2), 0);
+        assert_eq!(sl, 24);
+
+        let sl2 = sum_list2(Stepper::new(3, 10, 2), 0);
+
+        let fl = Stepper::new(4, 10, 2).fold(0,|acc,x|acc + x);
+        assert_eq!(fl, 18);
+    }
+}
